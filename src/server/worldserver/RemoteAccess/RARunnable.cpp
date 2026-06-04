e351e6318b74c36c5c106111509fb5e5ba88a658
AlterEgo <admin@projectskyfire.org>
2026-06-04 17:35:08 -0500

    Move RA accept loop to Boost.Asio async accept
    Signed-off-by: AlterEgo <admin@projectskyfire.org>

--- src/server/worldserver/RemoteAccess/RARunnable.cpp
+++ src/server/worldserver/RemoteAccess/RARunnable.cpp
@@ -16,12 +16,20 @@
 
 #include "RASocket.h"
 
+#include <boost/asio/error.hpp>
 #include <boost/asio/io_context.hpp>
+#include <boost/asio/steady_timer.hpp>
 #include <boost/system/error_code.hpp>
 #include <chrono>
 #include <memory>
-#include <thread>
 #include <utility>
+
+namespace
+{
+    void AsyncAcceptRA(std::shared_ptr<boost::asio::io_context> const& ioContext, std::shared_ptr<boost::asio::ip::tcp::acceptor> const& acceptor);
+    void ScheduleStopCheck(std::shared_ptr<boost::asio::io_context> const& ioContext, std::shared_ptr<boost::asio::ip::tcp::acceptor> const& acceptor,
+        std::shared_ptr<boost::asio::steady_timer> const& timer);
+}
 
 void RARunnable::Run()
 {
@@ -32,38 +40,77 @@ void RARunnable::Run()
     std::string stringIp = sConfigMgr->GetStringDefault("Ra.IP", "0.0.0.0");
 
     std::shared_ptr<boost::asio::io_context> ioContext(new boost::asio::io_context);
-    boost::asio::ip::tcp::acceptor acceptor(*ioContext);
-    if (!Skyfire::Net::OpenTcpAcceptor(*ioContext, acceptor, raPort, stringIp, "server.worldserver", "Skyfire RA"))
+    std::shared_ptr<boost::asio::ip::tcp::acceptor> acceptor(new boost::asio::ip::tcp::acceptor(*ioContext));
+    if (!Skyfire::Net::OpenTcpAcceptor(*ioContext, *acceptor, raPort, stringIp, "server.worldserver", "Skyfire RA"))
         return;
 
     SF_LOG_INFO("server.worldserver", "Starting Skyfire RA on port %d on %s", raPort, stringIp.c_str());
 
-    boost::system::error_code error;
-    while (!World::IsStopped())
-    {
-        error.clear();
-        std::unique_ptr<RASocketHandle> clientSocket(new RASocketHandle(*ioContext));
-        acceptor.accept(*clientSocket, error);
+    AsyncAcceptRA(ioContext, acceptor);
 
-        if (error)
-        {
-            if (!Skyfire::Net::IsWouldBlock(error))
-                SF_LOG_ERROR("commands.ra", "Skyfire RA failed to accept socket, error %d", error.value());
+    std::shared_ptr<boost::asio::steady_timer> stopTimer(new boost::asio::steady_timer(*ioContext));
+    ScheduleStopCheck(ioContext, acceptor, stopTimer);
 
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            continue;
-        }
-
-        boost::asio::ip::tcp::endpoint remoteEndpoint = clientSocket->remote_endpoint(error);
-        std::string remote = error ? std::string("<unknown>") : remoteEndpoint.address().to_string();
-
-        SF_LOG_INFO("commands.ra", "Incoming connection from %s", remote.c_str());
-
-        (new RASocket(ioContext, std::move(clientSocket), remote))->start();
-    }
+    ioContext->run();
 
     boost::system::error_code ignored;
-    acceptor.close(ignored);
+    acceptor->close(ignored);
 
     SF_LOG_DEBUG("server.worldserver", "Skyfire RA thread exiting");
+}
+
+namespace
+{
+    void AsyncAcceptRA(std::shared_ptr<boost::asio::io_context> const& ioContext, std::shared_ptr<boost::asio::ip::tcp::acceptor> const& acceptor)
+    {
+        if (!acceptor->is_open())
+            return;
+
+        std::shared_ptr<RASocketHandle> clientSocket(new RASocketHandle(*ioContext));
+        acceptor->async_accept(*clientSocket,
+            [ioContext, acceptor, clientSocket](boost::system::error_code const& error)
+            {
+                if (error)
+                {
+                    if (error != boost::asio::error::operation_aborted)
+                        SF_LOG_ERROR("commands.ra", "Skyfire RA failed to accept socket, error %d", error.value());
+                }
+                else
+                {
+                    boost::system::error_code endpointError;
+                    boost::asio::ip::tcp::endpoint remoteEndpoint = clientSocket->remote_endpoint(endpointError);
+                    std::string remote = endpointError ? std::string("<unknown>") : remoteEndpoint.address().to_string();
+
+                    SF_LOG_INFO("commands.ra", "Incoming connection from %s", remote.c_str());
+
+                    std::unique_ptr<RASocketHandle> socketHandle(new RASocketHandle(std::move(*clientSocket)));
+                    (new RASocket(ioContext, std::move(socketHandle), remote))->start();
+                }
+
+                if (!World::IsStopped())
+                    AsyncAcceptRA(ioContext, acceptor);
+            });
+    }
+
+    void ScheduleStopCheck(std::shared_ptr<boost::asio::io_context> const& ioContext, std::shared_ptr<boost::asio::ip::tcp::acceptor> const& acceptor,
+        std::shared_ptr<boost::asio::steady_timer> const& timer)
+    {
+        timer->expires_after(std::chrono::milliseconds(100));
+        timer->async_wait(
+            [ioContext, acceptor, timer](boost::system::error_code const& error)
+            {
+                if (error == boost::asio::error::operation_aborted)
+                    return;
+
+                if (World::IsStopped())
+                {
+                    boost::system::error_code ignored;
+                    acceptor->close(ignored);
+                    ioContext->stop();
+                    return;
+                }
+
+                ScheduleStopCheck(ioContext, acceptor, timer);
+            });
+    }
 }
